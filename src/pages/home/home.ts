47fd6505e2c918dced9ddce074409fdde8c3ac31
Sayuri Uyeda <sayuri_uyeda@hotmail.com>
2018-10-16 08:11:06 -0500

    app quiz

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BocinaPage } from '../bocina/bocina';
+import { TvPage } from '../tv/tv';
+import { SmartphonePage } from '../smartphone/smartphone';
+import { LamparaPage } from '../lampara/lampara';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+boc = BocinaPage;
+tele = TvPage;
+phone = SmartphonePage;
+lamp = LamparaPage;
   constructor(public navCtrl: NavController) {
 
   }
 
+  bocina() {
+  this.navCtrl.push(this.boc);
+  }
+
+  tv() {
+  this.navCtrl.push(this.tele);
+  }
+
+  smartphone() {
+  this.navCtrl.push(this.phone);
+  }
+
+  lampara() {
+  this.navCtrl.push(this.lamp);
+  }
 }
